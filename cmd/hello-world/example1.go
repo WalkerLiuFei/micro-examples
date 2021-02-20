8586d9723a64c544ae33ed4fe31233eca3361bc7
walkerliu <walkerliu1993@gmail.com>
2021-02-20 17:26:45 +0800

    init

---
+++ cmd/hello-world/example1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	greeter "github.com/WalkerLiuFei/micro-examples/proto/greeter"
+	"github.com/micro/go-micro"
+	"log"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *greeter.Request, rsp *greeter.Response) error {
+	rsp.Greeting = "Hello" + req.Name
+ 	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("helloworld"),
+		micro.Address("127.0.0.1:8888"),
+	)
+
+	service.Init()
+
+	greeter.RegisterGreeterHandler(service.Server(),new(Greeter))
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/WalkerLiuFei/micro-examples
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/micro/go-micro v1.18.0
+	google.golang.org/protobuf v1.25.0
+)
